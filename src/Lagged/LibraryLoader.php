145866ee0948122d92a9fa7a64d53a3ec57487b5
till <till@php.net>
2011-09-28 14:10:53 +0200

    * add phpdoc for all methods and class variables  * allow injection of app dir location (via __construct)  * APPLICATION_PATH >> LAGGED_APPLICATION_PATH  * make directory var 'static' in load()

--- src/Lagged/LibraryLoader.php
+++ src/Lagged/LibraryLoader.php
@@ -36,16 +36,47 @@
 
 class Lagged_LibraryLoader
 {
+    /**
+     * @var string $appDir Application directory (root)
+     */
+    protected $appDir;
+
+    /**
+     * @var string $module Name of the module which includes the library folder.
+     */
     protected $module;
-    
-    public function __construct($module)
+
+    /**
+     * __construct
+     *
+     * @param string $module
+     * @param mixed  $appDir If 'null', LAGGED_APPLICATION_PATH is used.
+     *
+     * @return $this
+     */
+    public function __construct($module, $appDir = null)
     {
-        $this->module = $module;
+        $this->module = strtolower($module);
+
+        if ($appDir === null) {
+            $this->appDir = LAGGED_APPLICATION_PATH;
+        }
     }
 
+    /**
+     * Autoloader!
+     *
+     * @param string $className
+     *
+     * @return boolean
+     */
     public function load($className)
-    {   
-        $moduleLibraryPath = APPLICATION_PATH . '/modules/'.strtolower($this->module).'/library/';
+    {
+        static $moduleLibraryPath;
+        if ($moduleLibraryPath === null) {
+            $moduleLibraryPath = $this->appDir . '/modules/' . $this->module . '/library/';
+        }
+
         $file = str_replace('_', '/', $className) . '.php';
         return include $moduleLibraryPath . $file;
     }
