5b7cba6721bb737606a13cd1c6c94a4d30e5af5c
till <till@php.net>
2011-09-28 19:21:01 +0200

    small bugfixes to make these scripts run

---
+++ scripts/setup.php
@@ -0,0 +1,27 @@
+<?php
+$include_path  = '';
+$include_path .= $_SERVER['HOME'] . '/Documents/workspaces/imagineeasy_v5/trunk/library:';
+$include_path .= '.';
+
+set_include_path($include_path . ':' . get_include_path());
+
+include '../src/Lagged/Loader.php';
+
+$libs   = array();
+$libs[] = 'Zend_Auth';
+$libs[] = 'Zend_Config_Ini';
+$libs[] = 'Zend_Controller_Action';
+$libs[] = 'Zend_Controller_Action_HelperBroker';
+$libs[] = 'Zend_Controller_Front';
+$libs[] = 'Zend_Controller_Plugin_ErrorHandler';
+$libs[] = 'Zend_Controller_Router_Route';
+$libs[] = 'Zend_Db';
+$libs[] = 'Zend_Db_Table_Abstract';
+$libs[] = 'Zend_Http_Client';
+$libs[] = 'Zend_Mail_Transport_Smtp';
+$libs[] = 'Zend_Registry';
+$libs[] = 'Zend_Session';
+$libs[] = 'Zend_Session_Namespace';
+$libs[] = 'Zend_View';
+$libs[] = 'Zend_XmlRpc_Client';
+$libs[] = 'Zend_XmlRpc_Server';
--- scripts/zend.php
+++ scripts/zend.php
@@ -1,31 +1,7 @@
 <?php
-// ZendFramework 1.7.6
-$include_path  = '/usr/local/share/pear:';
-$include_path .= '/usr/home/till/public_html/zf-lib/:';
-$include_path .= '.';
+include __DIR__ . '/setup.php';
 
-set_include_path($include_path);
-
-include 'Zend/Loader.php';
-
-$libs   = array();
-$libs[] = 'Zend_Auth';
-$libs[] = 'Zend_Config_Ini';
-$libs[] = 'Zend_Controller_Action';
-$libs[] = 'Zend_Controller_Action_HelperBroker';
-$libs[] = 'Zend_Controller_Front';
-$libs[] = 'Zend_Controller_Plugin_ErrorHandler';
-$libs[] = 'Zend_Controller_Router_Route';
-$libs[] = 'Zend_Db';
-$libs[] = 'Zend_Db_Table_Abstract';
-$libs[] = 'Zend_Http_Client';
-$libs[] = 'Zend_Mail_Transport_Smtp';
-$libs[] = 'Zend_Registry';
-$libs[] = 'Zend_Session';
-$libs[] = 'Zend_Session_Namespace';
-$libs[] = 'Zend_View';
-$libs[] = 'Zend_XmlRpc_Client';
-$libs[] = 'Zend_XmlRpc_Server';
+require $_SERVER['HOME'] . '/Documents/workspaces/imagineeasy_v5/trunk/library/Zend/Loader.php';
 
 $start = microtime(true);
 $data  = array();
